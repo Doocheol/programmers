b86b120a9a186395c7813689bdab081d28a16514
Doocheol <encjf2070@gmail.com>
2025-01-06 17:37:41 +0900

    [level 1] Title: 예산, Time: 0.58 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        
+        Arrays.sort(d);
+        int count = 0;
+
+        
+        for (int amount : d) {
+            if (budget < amount) {
+                break;
+            }
+            
+            budget -= amount;
+            count++;
+        }
+        return budget >= 0 ? count : count - 1;
+        
+    }
+}
