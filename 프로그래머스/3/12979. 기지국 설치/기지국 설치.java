f107e40ebd597dde85be34eefbd19c1d2edf0e67
Doocheol <encjf2070@gmail.com>
2025-01-07 14:37:13 +0900

    [level 3] Title: 기지국 설치, Time: 1.04 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,21 +1,22 @@
 class Solution {
     public int solution(int n, int[] stations, int w) {
+        
         int answer = 0;
         int location = 1;
         int idx = 0;
         
         while (location <= n) {
-            if (idx < stations.length && location >= stations[idx] - w) {
+            if (idx < stations.length && stations[idx] - w <= location) {
                 location = stations[idx] + w + 1;
                 idx++;
             }
             else {
-                location += 2 * w + 1;
+                location += w * 2 + 1;
                 answer++;
             }
         }
-    
-    
+        
         return answer;
+        
     }
 }
